63ab1a4e7c2b8f3bbf36452abc2a5a26e6525239
Randall J Fernandez <70555786+randallfernandez@users.noreply.github.com>
2022-12-13 17:45:42 -0800

    modes via LED

--- system/SwampCooler.ino
+++ system/SwampCooler.ino
@@ -79,178 +79,172 @@ void writeReg()
   digitalWrite(clockPin, HIGH);
 }
 
-//test
-
 void setup() {
-    // setup the UART
-    Serial.begin(9600);
 
-    // setup the ADC
-    adcInit();
+  lcd.begin(16, 2);                                                 //LCD display
+  
+  pinMode(motor, OUTPUT);                                           //motor
+  pinMode(motorIN_1, OUTPUT);                                       //direction of motor
+  pinMode(motorIN_2, OUTPUT);
+  digitalWrite(motorIN_1, LOW);                                     // motor should be off initially
+  digitalWrite(motorIN_2, LOW);
 
-    //setup the Servo
-    servo.attach(9);
-    servo.write(fanAngle);
+  pinMode(latchPin,  OUTPUT);                                       //  latch pin 
+  pinMode(dataPin,  OUTPUT);                                        //  data pin 
+  pinMode(clockPin, OUTPUT);                                        // clock pin o
+  pinMode(buttonPin, INPUT);                                        // push button
 
-    //setup the fan
-    pinModeB(FAN_ENABLE, OUTPUT);
-    pinModeB(DIRA, OUTPUT);
-    pinModeB(DIRB, OUTPUT);
-    digitalWriteB(DIRA, HIGH);
+  bool parse=false;                                                 // boolean variables used for time functions
+  bool config=false;
 
-    //setup on/off button
-    pinModeB(SYSTEM, INPUT_PULLUP);
 
-    // setup the LCD:
-    lcd.init();
-    lcd.backlight();
+  if (getDate(__DATE__) && getTime(__TIME__)) {                     // record the date and time
+    parse = true;
+    if (RTC.write(tm)) {
+      config = true;
+     }
+    }
 
-    Serial.print("Set Fan Angle (45 - 135)\n");
-}
+    Serial.begin(9600);       
+    while (!Serial) ;
+    // print the date and time 
+    lcd.setCursor(0,0);
+    lcd.print("Time is: ");
+    lcd.setCursor(0,1);
+    lcd.print(__TIME__);
+    delay(2000);
+    lcd.setCursor(0,0);
+    lcd.print("Today's date:");
+    lcd.setCursor(0,1);
+    lcd.print(__DATE__);
+    delay(2000);
+    lcd.clear();
+  
+} // end of initial setup
 
 void loop() {
-    lastButtonState = currentButtonState;      // save the last state
-    currentButtonState = digitalReadB(SYSTEM); // read new state
-
-    if(lastButtonState == HIGH && currentButtonState == LOW) {      
-        enable = !enable;
-        if(enable) {
-            Serial.println("System On");
-        }
-        else {
-            Serial.println("System Off");
-
-            lcd.setCursor(0,0);
-            lcd.print(".               ");
-
-            lcd.setCursor(0, 1);
-            lcd.print(".               ");
-
-            digitalWriteB(FAN_ENABLE, LOW);
-        }
-    }
-    if(enable) {
-        
-        waterLevel = adcRead(WaterSensorPin);
-        
-        if(Serial.available() > 0) {
-            String fanAngleS = Serial.readString();
-            fanAngle = fanAngleS.toInt();
-            Serial.print(fanAngleS);
-            if(fanAngle <= 135 && fanAngle >= 45) {
-                servo.write(fanAngle);
-                Serial.print("Set Fan Angle (45 - 135)\n");
-            }
-        }
-        
-        if(((HistoryValue >= waterLevel) && ((HistoryValue - waterLevel) > 10)) || ((HistoryValue < waterLevel) && ((waterLevel - HistoryValue) > 10))) {
-            // Serial.println("Water Level: " + waterLevel);
-            HistoryValue = waterLevel;
-        }
-
-        /* Measure temperature and humidity.  If the functions returns
-            true, then a measurement is available. */
-        //if(measure_environment(&temperature, &humidity) == true) {
-        if(dht_sensor.measure(&temperature, &humidity)) {
-            lcd.setCursor(0,0);
-            lcd.print("Temp      C     ");
-            lcd.setCursor(6,0);
-            lcd.print(temperature, 1);
-
-            lcd.setCursor(0, 1);
-            lcd.print("%Humidity       ");
-            lcd.setCursor(10,1);
-            lcd.print(humidity, 1);
-        }
-
-        if(temperature >= 21 || temperature <= 16) {
-            digitalWriteB(FAN_ENABLE, HIGH);
-        }
-        else {
-            digitalWriteB(FAN_ENABLE, LOW);
-        }
-
-        if(waterLevel < 40) {
-            enable = 0;
-            lcd.setCursor(0,0);
-            lcd.print("ERROR           ");
-
-            lcd.setCursor(0, 1);
-            lcd.print("Water Level Low ");
-            digitalWriteB(FAN_ENABLE, LOW);
-        }
-    }
-}
-
-void pinModeB(uint8_t pin, uint8_t mode) {
-    if(mode == INPUT) {
-        *ddrB &= ~(INPUT << pin);
-    }
-    else if(mode == OUTPUT) {
-        *ddrB |= OUTPUT << pin;
-    }
-    else if(mode == INPUT_PULLUP) {
-        *portB |= 0x1 << pin;
-    }
-}
-
-void digitalWriteB(uint8_t pin, uint8_t val) {
-    if(val == LOW) {
-        *portB &= ~(0x01 << pin);
-    }
-    else if(val == HIGH) {
-        *portB |= 0x01 << pin;
-    }
-}
-
-bool digitalReadB(uint8_t pin) {
-    if(*pinB & (0x1 << pin)) return HIGH;
-    return LOW;
-}
-
-void adcInit() {
-    // set up the A register
-  *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
-  *my_ADCSRA &= 0b11011111; // clear bit 5 to 0 to disable the ADC trigger mode
-  *my_ADCSRA &= 0b11110111; // clear bit 3 to 0 to disable the ADC interrupt
-  *my_ADCSRA &= 0b11111000; // clear bit 2-0 to 0 to set prescaler selection to slow reading
+  // begin program
   
-  // set up the B register
-  *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
-  *my_ADCSRB &= 0b11110111; // clear bit 2-0 to 0 to set free running mode
+    digitalWrite(latchPin, LOW);
+    digitalWrite(dataPin, shiftRegister[2]);
+    digitalWrite(latchPin, HIGH);
+ 
+  buttonState = digitalRead(buttonPin);                             //on and off
+  buttonState = HIGH;
+ 
+  waterLevel = analogRead(Spin);                                    //water sensor analog pin                                     
+
+
+  //Humidity/Temperature
+  DHT.read11(A2);
+  lcd.setCursor(0,0); 
+  lcd.print("Temp: ");
+  lcd.print(DHT.temperature);
+  lcd.print((char)223);                  
+  lcd.print("C");
+  lcd.setCursor(0,1); 
+  lcd.print("Temp: ");
+  lcd.print(DHT.temperature*9/5 + 32 );   
+  lcd.print((char)223);                   
+  lcd.print("F");
+  delay(3000);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Humidity: ");
+  lcd.setCursor(0,1);
+  lcd.print(DHT.humidity);
+  lcd.print("%");
+  delay(3000);
+  lcd.clear();
   
-  // set up the MUX Register
-  *my_ADMUX  &= 0b00000000; // clear bit 7 to 0 for AVCC analog reference
-  *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
-  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
-  *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bits
-}
-
-unsigned int adcRead(unsigned char adc_channel_num) {
-    // reset the channel and gain bits
-    *my_ADMUX &= 0b11100000;
-
-    // clear the channel selection bits
-    *my_ADCSRB &= 0b11110111;
-
-    // set the channel number
-    if(adc_channel_num > 7) {
-        // set the channel selection bits, but remove the most significant bit (bit 3)
-        adc_channel_num -= 8;
-        
-        // set MUX bit 
-        *my_ADCSRB |= 0b00001000;
-    }
-
-    // set the channel selection bits
-    *my_ADMUX  += adc_channel_num;
-
-    // set bit ?? of ADCSRA to 1 to start a conversion
-    *my_ADCSRA |= 0b01000000;
   
-    // wait for the conversion to complete
-    while(*my_ADCSRA & 0b000000000);
-    
-    // return the result in the ADC data register
-    return *my_ADC_DATA;
-}
+  // Motor threshold test code mock up
+  if(DHT.temperature >= 20)                                         // when the temperature is above 20 degress Celsius,
+  {                                                                // motor will pick up speed
+    digitalWrite(motor, 200);
+    digitalWrite(motorIN_1, HIGH);
+    digitalWrite(motorIN_2, LOW);
+  }
+  else                                                             // else, motor will still run around 100 when idle
+  {
+    digitalWrite(motor, 0);
+    digitalWrite(motorIN_1, HIGH);
+    digitalWrite(motorIN_2, LOW);
+  }
+
+  if(buttonState == LOW)                      //disabled
+  {
+    lcd.setCursor(0,0);                                               // display system state indicator on LCD
+    lcd.print("SWAMP COOLER");
+    lcd.setCursor(0,1);
+    lcd.print("MODE: DISABLED");
+    delay(3000);
+    lcd.clear();
+    //bitSet(leds, ledPinYellow);                                       // Turn on Yellow LED 
+    //updateShiftRegister();
+    //bitClear(leds, 0);                                              // Turn off other LEDs
+    //updateShiftRegister();                                          // reflect the state of the LEDs
+    digitalWrite(motor, 0);                                           // turn off motor
+    digitalWrite(motorIN_1, LOW);                                     
+    digitalWrite(motorIN_2, LOW);
+  }
+  //else                                        //idle
+  //{
+    //bitSet(leds, ledPinGreen);                                        // Turn on Green LED for Idle until cooler starts running
+    //bitClear(leds, 0);                                                // Turn off other LEDs
+    //updateShiftRegister();                                            // reflect the state of the LEDs
+  //}
+
+  
+  if(waterLevel <= 100)                       //error           
+  {
+         //bitSet(leds, ledPinRed);                                     // Turn on Red LED
+         //bitClear(leds, 0);                                           // clear other LEDs
+         //updateShiftRegister();                                       // reflect the state of the LEDs
+
+         digitalWrite(motor, 0);                                      // 0 is the motor not moving, change up to 255 to switch
+         digitalWrite(motorIN_1, LOW);                                // to switch the speed
+         digitalWrite(motorIN_2, LOW);
+         
+         lcd.setCursor(0,0);
+         lcd.print("ERROR: WATER");                             // indicate water level on LCD display
+         lcd.setCursor(0,1);
+         lcd.print("LEVEL IS LOW");
+         delay(3000);
+         lcd.clear();
+  }
+  
+  else if (waterLevel > 100 && waterLevel <= 300){
+         lcd.print("WATER: LOW");                             // indicate water level on LCD display
+         delay(3000);
+         lcd.clear();                                // indicate other water level ranges on display
+  }
+  else if (waterLevel > 300 && waterLevel <= 330){
+         lcd.print("WATER: MEDIUM");
+         delay(3000);
+         lcd.clear();
+  }
+  else if (waterLevel > 330){
+         lcd.print("WATER: HIGH");
+         delay(3000);
+         lcd.clear();
+  }
+  //delay(3000);
+  
+
+  if(DHT.temperature > 30)                     //run
+  {
+     lcd.setCursor(0,0);                                             // display system indicator on LCD
+     lcd.print("SWAMP COOLER");
+     lcd.setCursor(0,1);
+     lcd.print("MODE: RUNNING");
+     delay(3000);
+     lcd.clear();
+     //bitSet(leds, ledPinBlue);                                       // Blue LED for running
+     //bitClear(leds, 0);                                              // Clear rest of LEDs
+     //updateShiftRegister();                                          // reflect the state of the LEDs
+  }
+
+  
+  //delay(3000);                                                      //setup normal delay
+} // end of loop
